9926150b6d3ce10f270e8267c6ff2248c1749fb9
Vincent van Proosdij <mini32@gmail.com>
2018-10-09 16:43:40 +0200

    init

---
+++ tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace Naos.Reference.UnitTests
+{
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // TODO: implement
+        }
+    }
+}
